e18f6498a16595cd43897aec8e7b896b0c049a1f
Andres Vega <gydoar@hotmail.com>
2015-05-08 16:39:37 -0500

    agregando style de google traslat

--- page-contacto.php
+++ page-contacto.php
@@ -1,3 +1,56 @@
+   <?php
+   if(isset($_POST['submitted'])) {
+    if(trim($_POST['contactName']) === '') {
+        $nameError = 'Por favor ingrese su nombre.';
+        $hasError = true;
+    } else {
+        $name = trim($_POST['contactName']);
+    }
+
+    if(trim($_POST['email']) === '')  {
+        $emailError = 'Por favor ingrese su email.';
+        $hasError = true;
+    } else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
+        $emailError = 'Dirreción de email invalida.';
+        $hasError = true;
+    } else {
+        $email = trim($_POST['email']);
+    }
+
+     if(trim($_POST['telefono']) === '') {
+        $telError = 'Por favor ingrese su teléfono.';
+        $hasError = true;
+    } else {
+        $telefono = trim($_POST['telefono']);
+    }
+
+    if(trim($_POST['comments']) === '') {
+        $commentError = 'Por favor ingresa un mensaje.';
+        $hasError = true;
+    } else {
+        if(function_exists('stripslashes')) {
+            $comments = stripslashes(trim($_POST['comments']));
+        } else {
+            $comments = trim($_POST['comments']);
+        }
+    }
+
+    if(!isset($hasError)) {
+        $emailTo = get_option('tz_email');
+        if (!isset($emailTo) || ($emailTo == '') ){
+            $emailTo = get_option('admin_email');
+        }
+        $subject = '[Contacto axemos] From '.$name;
+        $body = "Nombre: $name \n\nEmail: $email \n\nTeléfino: $telefono \n\nComments: $comments";
+        $headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+        wp_mail($emailTo, $subject, $body, $headers);
+        $emailSent = true;
+    }
+
+} ?>
+
+
 <?php get_header(); ?>
 
 	<article class="page">
@@ -19,7 +72,70 @@
 					<?php endif; ?>
 					
 				
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam error deserunt dolorem soluta enim dolor atque ex consectetur earum aperiam magni omnis laudantium vel necessitatibus accusantium, mollitia voluptatum autem dolores.
+				 <div class="six columns">
+
+             <?php if(isset($emailSent) && $emailSent == true) { ?>
+             <div class="alert alert-success">
+                <p>Gracias, su mensaje ha sido bien recibido.</p>
+            </div>
+            <?php } else { ?>
+
+            <?php if(isset($hasError) || isset($captchaError)) { ?>
+            <div class="alert alert-danger alert-dismissable">
+                <a class="close" data-dismiss="alert">×</a>
+                <h4 class="alert-heading">Disculpe, existen algunos errores.</h4>
+                <p class="error">Por favor inténtalo de nuevo!<p>
+                </div>
+                <?php } ?>
+
+                <form action="<?php the_permalink(); ?>" id="contactForm" method="post">
+                    <fieldset>
+                        <div class="form-group">
+
+                            <input class="form-control" type="text" name="contactName" id="contactName" value="" placeholder="Nombre" />
+                            <span class="input-icon fui-check-inverted"></span>
+                            <?php if(isset($nameError)) { ?>
+                            <p><span class="error"><?=$nameError;?></span></p>
+                            <?php } ?>
+                             <br>
+
+                        </div>
+                        <div class="form-group">
+
+                            <input class="form-control" type="text" name="email" id="email" value="" placeholder="Email" />
+                            <span class="input-icon fui-check-inverted"></span>
+                            <?php if(isset($emailError)) { ?>
+                            <p><span class="error"><?=$emailError;?></span></p>
+                            <?php } ?>
+
+                             <br>
+                        </div>
+                        <div class="form-group">
+
+                            <input class="form-control" type="text" name="telefono" id="telefono" value="" placeholder="Teléfono" />
+                            <span class="input-icon fui-check-inverted"></span>
+                            <?php if(isset($telError)) { ?>
+                            <p><span class="error"><?=$telError;?></span></p>
+                            <?php } ?>
+
+                             <br>
+                        </div>
+                        <div class="form-group">
+
+                            <textarea class="form-control" name="comments" id="commentsText" rows="100" cols="10" placeholder="Mensaje"></textarea>
+                            <?php if(isset($commentError)) { ?>
+                            <p><span class="error"><?=$commentError;?></span></p>
+                            <?php } ?>
+                             <br>
+                        </div>
+                        <div class="form-actions">
+                            <button style="float:right;"type="submit" class="btn btn-success button-primary">Enviar</button>
+                        </div>
+                        <input type="hidden" name="submitted" id="submitted" value="true" />
+                    </fieldset>
+                </form>
+                <?php } ?>
+            </div>
 				</div>
 					
 			</div>
