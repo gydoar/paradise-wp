fd858b26669147fecc542c4fcf8429e9fe0a6f77
Andres Vega <gydoar@hotmail.com>
2015-05-11 23:20:03 -0500

    agregando cambios al widget 2

--- footer-widget.php
+++ footer-widget.php
@@ -12,7 +12,7 @@
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<div class="row">
 				<div class="three columns">
-					<?php if (has_post_thumbnail()) {the_post_thumbnail('noticias_widget');} ?>
+					<a href="<?php the_permalink(); ?>"><?php if (has_post_thumbnail()) {the_post_thumbnail('noticias_widget');} ?></a>
 				</div>
 					
 				<div class="nine columns">
