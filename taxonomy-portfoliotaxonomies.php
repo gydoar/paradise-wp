bfb0acf19775a8ab8a4b0762b792a2f109f76b1d
Andres Vega <gydoar@hotmail.com>
2015-07-12 21:19:39 -0500

    agregando page nav

--- taxonomy-portfoliotaxonomies.php
+++ taxonomy-portfoliotaxonomies.php
@@ -13,69 +13,21 @@
 					<!-- Bloque 1 -->
 					<?php rewind_posts(); ?>
 					<?php global $wp_query;
-					$wp_query = new WP_Query("post_type=portfolio&post_status=publish&posts_per_page=4");
+					$wp_query = new WP_Query("post_type=portfolio&post_status=publish&posts_per_page=-1");
 					
 					while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-						<div class="three columns">
+						<div class="tax__img">
 							<a href="<?php the_permalink(); ?>">
 								<?php if (has_post_thumbnail()) {the_post_thumbnail('img_1');} ?>
 								<div class="caja__nombre">
 									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 								</div>
-							</a>	
-
+							</a>								
 						</div>
-
-					<?php endwhile; ?>
-					<div>
-						
-					
-					<!-- Bloque 2 -->
-					<?php rewind_posts(); ?>
-					<?php global $wp_query;
-					$wp_query = new WP_Query("post_type=portfolio&post_status=publish&posts_per_page=8&showposts=4&offset=4");
-					
-					while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-						<div class="three columns">
-							<a href="<?php the_permalink(); ?>">
-								<?php if (has_post_thumbnail()) {the_post_thumbnail('img_1');} ?>
-								<div class="caja__nombre">
-									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-								</div>
-							</a>	
-
-						</div>
-
-					<?php endwhile; ?>
-					</div>
-
-					<div>
-
-					<!-- Bloque 3 -->
-					<?php rewind_posts(); ?>
-					<?php global $wp_query;
-					$wp_query = new WP_Query("post_type=portfolio&post_status=publish&posts_per_page=-1&showposts=4&offset=8");
-					
-					while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-						<div class="three columns">
-							<a href="<?php the_permalink(); ?>">
-								<?php if (has_post_thumbnail()) {the_post_thumbnail('img_1');} ?>
-								<div class="caja__nombre">
-									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-								</div>
-							</a>	
-
-						</div>
-
+					<?php wp_pagenavi(); ?>
 					<?php endwhile; ?>
 				</div>
-				</div>
-
-
-				<!-- Bloque 2 -->
 
 				<div class="ornament-top"><img width="270" src="<?php bloginfo('template_url' ); ?>/library/img/ornament-bottom.png" alt="">
 				</div>
